8f7d6693df4e735b8550a8399adf74f7cc12d822
Souvik Basu <bsouvik986.com>
2026-02-17 21:52:27 +0530

    feat: Add `https://adhyayan-trial.netlify.app` to allowed CORS origins.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://adhyayan-trial.netlify.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
